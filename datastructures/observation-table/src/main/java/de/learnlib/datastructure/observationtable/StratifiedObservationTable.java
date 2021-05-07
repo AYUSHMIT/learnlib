11738c4d17f62e018bcab09d8e6c35513dc4353d
Gaëtan Staquet <gaetan.staquet@gmail.com>
2021-05-07 16:02:21 +0200

    Removed useless import

--- datastructures/observation-table/src/main/java/de/learnlib/datastructure/observationtable/StratifiedObservationTable.java
+++ datastructures/observation-table/src/main/java/de/learnlib/datastructure/observationtable/StratifiedObservationTable.java
@@ -2,7 +2,6 @@ package de.learnlib.datastructure.observationtable;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import net.automatalib.words.Word;
